5d00b25433a0f9b649a8cc3f781e490b3a8985c3
restlucas <lucasouza.doliveira@gmail.com>
2025-01-21 22:26:15 -0300

    fix: window use correction

--- src/utils/useTheme.ts
+++ src/utils/useTheme.ts
@@ -4,15 +4,17 @@ export const useSystemTheme = (): "light" | "dark" => {
   const [systemTheme, setSystemTheme] = useState<"light" | "dark">("light");
 
   useEffect(() => {
-    const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
-    setSystemTheme(darkMode.matches ? "dark" : "light");
+    if (typeof window !== "undefined") {
+      const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+      setSystemTheme(darkMode.matches ? "dark" : "light");
 
-    const listener = (e: MediaQueryListEvent) => {
-      setSystemTheme(e.matches ? "dark" : "light");
-    };
+      const listener = (e: MediaQueryListEvent) => {
+        setSystemTheme(e.matches ? "dark" : "light");
+      };
 
-    darkMode.addEventListener("change", listener);
-    return () => darkMode.removeEventListener("change", listener);
+      darkMode.addEventListener("change", listener);
+      return () => darkMode.removeEventListener("change", listener);
+    }
   }, []);
 
   return systemTheme;
